20b8e0db36b0ea5590ba3b1ee75560f52d701ceb
Ivan Rodrigues <ivanknow@gmail.com>
2016-02-17 10:13:18 -0300

    Usando local storage no navegador

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,39 +1,57 @@
 angular.module('minhasContas.controllers', [])
 
-.controller('minhasContasCtrl', function($scope) {
-	var vm = this;
-	vm.total = 0;
-	vm.itemAtual = {};
-	vm.listaValores = [];
-	vm.addValor = function() {
-
-		vm.listaValores.push(vm.itemAtual);
-		vm.itemAtual = {};
-		vm.getSaldo();
-
-	};
-
-	vm.removeValor = function(index) {
-
-		if (index > -1) {
-			vm.listaValores.splice(index, 1);
-		}
-		vm.getSaldo();
-		
-	};
-
-	vm.getSaldo = function() {
-		if (vm.listaValores.length == 0) {
+.controller(
+		'minhasContasCtrl',
+		function($scope) {
+			var vm = this;
 			vm.total = 0;
+			vm.itemAtual = {};
+			if (localStorage.getItem('items')) {
+				vm.listaValores = JSON.parse(localStorage.items);
+				;
+			} else {
+				vm.listaValores = [];
+			}
 
-		}
-		vm.total = 0;
-		for (var i = 0; i < vm.listaValores.length; i++) {
-			vm.total += parseFloat(vm.listaValores[i].valor);
+			vm.addValor = function() {
 
-		}
+				vm.listaValores.push(vm.itemAtual);
+				vm.itemAtual = {};
+				vm.getSaldo();
+				localStorage.setItem('items', JSON.stringify(vm.listaValores));
 
-	};
+			};
 
-	vm.getSaldo();
-})
+			vm.removeValor = function(index) {
+
+				if (index > -1) {
+					vm.listaValores.splice(index, 1);
+				}
+				vm.getSaldo();
+
+			};
+			
+			vm.removeAll = function() {
+
+				
+					vm.listaValores=[];
+					localStorage.setItem('items','');
+					vm.getSaldo();
+
+			};
+
+			vm.getSaldo = function() {
+				if (vm.listaValores.length == 0) {
+					vm.total = 0;
+
+				}
+				vm.total = 0;
+				for (var i = 0; i < vm.listaValores.length; i++) {
+					vm.total += parseFloat(vm.listaValores[i].valor);
+
+				}
+
+			};
+
+			vm.getSaldo();
+		})
